a3548defbbc16fdd5286eca6da1d49517cef2fc3
Bhavik Shah <bhavik.shah@sts.local>
2022-07-15 13:57:01 +0530

    code test now 13.56

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # give permission to the files inside /secure_docs directory
-sudo chmod -R 777 /home/ubuntu/blog_app
+sudo chmod -R 777 /home/ubuntu/simple-nodejs-app
 # navigate into current working directory
 cd /home/ubuntu/blog_app
 # install node modules
