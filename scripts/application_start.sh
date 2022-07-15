107049c4a6b1977bfac4e599b585bf7130f74372
Bhavik Shah <bhavik.shah@sts.local>
2022-07-15 14:02:46 +0530

    code changed at 14.02

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,7 +2,7 @@
 # give permission to the files inside /secure_docs directory
 sudo chmod -R 777 /home/ubuntu/simple-nodejs-app
 # navigate into current working directory
-cd /home/ubuntu/blog_app
+cd /home/ubuntu/simple-nodejs-app
 # install node modules
 npm install
 # start our node app in the background using pm2
